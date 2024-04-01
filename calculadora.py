bd5a445b8bc5c128ade09e20b5f73cad899f5b51
jelen12 <jelen1029@gmail.com>
2024-04-01 09:04:16 -0500

    menu

--- calculadora.py
+++ calculadora.py
@@ -20,3 +20,16 @@ print("Ingrese 2 números")
 
 num1= int(input())
 num2= int(input())
+
+operacion=int(input("Elija la operación que desea realizar:\n1.Suma \n2.Resta \n3.Multiplicación \n4.Division\n"))
+
+if(operacion==1):
+    print(f"El resultado de la suma es {suma(num1,num2)}")
+elif(operacion==2):
+    print(f"El resultado de la resta es {resta(num1,num2)}")
+elif(operacion==3):
+    print(f"El resultado de la multiplicación es {multiplicacion(num1,num2)}")
+elif(operacion==4):
+    print(f"El resultado de la división es {division(num1,num2)}")
+else:
+    print("Opción incorrecta")
